335248533eb33bd6779ef1732ab08f8e63fbf96d
Kusoo <807128830@qq.com>
2016-11-03 13:08:34 +0800

    ADD nio-httpserver

--- src/main/java/edu/nju/collapsar/Worker.java
+++ src/main/java/edu/nju/collapsar/Worker.java
@@ -1,23 +1,42 @@
 package edu.nju.collapsar;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
-/**
- * Created by rico on 2016/11/2.
- */
-public class Worker implements Runnable{
+public class Worker implements Runnable {
+    private final ByteBuffer buffer = ByteBuffer.allocate(2048);
+    private SocketChannel socketChannel;
+    private Charset charset = Charset.forName("UTF-8");
 
-    private Socket socket = null;
-    private Request request = null;
-
-    public Worker(Socket socket){
-        this.socket = socket;
+    public Worker(SocketChannel socketChannel) {
+        this.socketChannel = socketChannel;
     }
 
     public void run() {
+        buffer.clear();
+        try {
+            socketChannel.read(buffer);
+            String requestStr = new String(buffer.array());
+
+            //response to not file-request
+            Response response = new Response() {
+                public void write(String content) {
+                    try {
+                        socketChannel.write(charset.encode(CharBuffer.wrap(content)));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            socketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
 
 }
